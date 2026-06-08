dc28382e92aaf00802bf21c58cfc8763f199c1a6
bizhang90 <tr.ngochien@gmail.com>
2026-06-08 16:54:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  mode: 'demo', // đổi thành 'supabase' khi vận hành online
-  supabaseUrl: '',
-  supabaseAnonKey: ''
+  mode: 'supabase',
+  supabaseUrl: 'https://eoduvgpxnlnizdafdlby.supabase.co',
+  supabaseAnonKey: 'sb_publishable_smJg69GBmmrx0avWvsE7Xw_9tro3sV0'
 };
